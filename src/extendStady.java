e4b99f4374f9203d960a4fcbdedde7ff97afa4e2
주동호 <100278301+JOODON@users.noreply.github.com>
2022-05-01 23:44:40 +0900

    JAVA연습파일들

---
+++ src/extendStady.java
@@ -0,0 +1,26 @@
+public class extendStady {
+    public static void main(String[]args){
+        Wizard wizard=new Wizard("해리포터",100,100);
+        Wizard.toString();
+        Novice novice=new Novice("전사",10);
+        Novice.toString();
+    }
+}
+class Novice{
+    private String name;
+    private int hp;
+public Novice(String name,int hp){
+    this.name=name;
+    this.hp=hp;
+}
+public String toString(){
+    return String.format("%s(HP:%d)",this.name,this.hp);
+ }
+}
+class Wizard extends Novice{
+    private int mp;
+    public Wizard(String name,int hp,int mp){
+        super(name,hp);//기존에 있었던 이름이랑 hp를 선언
+        this.mp=mp;
+    }
+}
